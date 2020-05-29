fde2053051a6c4f52328233804012bd4204a55b9
Potewo <potewo@protonmail.com>
2020-05-29 12:48:45 +0000

    checked in docker container

--- install.sh
+++ install.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 # https://qiita.com/yutakatay/items/c6c7584d9795799ee164
+echo "start"
 set -ue
-
+echo "1"
 helpmsg() {
   command echo "Usage: $0 [--help | -h]" 0>&2
   command echo ""
 }
-
+echo "2"
+# set -ue <-> command exist check
 install_starship() {
+  echo "a"
   type starship > /dev/null 2>&1
+  echo "b"
   if [ $? -eq 0 ] ; then # コマンドが存在すれば
+    echo "c"
     command echo "starship is already exist. skipped installing."
+    echo "d"
   else
+    echo "e"
     command echo "starship is not found. start installing."
+    echo "f"
     curl -fsSL https://starship.rs/install.sh | bash
+    echo "g"
   fi
 }
-
+echo "3"
 install_zinit() {
   type starship > /dev/null 2>&1
   if [ $? -eq 0 ] ; then # コマンドが存在すれば
@@ -26,7 +35,7 @@ install_zinit() {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
   fi
 }
-
+echo "4"
 link_to_homedir() {
 
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
@@ -57,7 +66,7 @@ link_to_homedir() {
     command echo "same install src dest"
   fi
 }
-
+echo "5"
 while [ $# -gt 0 ];do
   case ${1} in
     --debug|-d)
@@ -72,9 +81,12 @@ while [ $# -gt 0 ];do
   esac
   shift
 done
-
+echo "6"
 install_starship
+echo "7"
 install_zinit
+echo "8"
 link_to_homedir
+echo "9"
 command echo -e "\e[1;36m Install completed!!!! \e[m"
-
+echo "10"
